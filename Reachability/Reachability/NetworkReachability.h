524e8530f0c47d901c40da4edcf548d636e8d6ad
孙浩 <sunhao@rongcloud.cn>
2018-12-21 14:31:18 +0800

    feat：添加注释

--- Reachability/Reachability/NetworkReachability.h
+++ Reachability/Reachability/NetworkReachability.h
@@ -32,14 +32,49 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, copy) NetworkStatusChange networkStatusChange;
 
+
+/**
+ 通过默认路由地址初始化
+
+ @return NetworkReachability
+ */
 + (instancetype)reachabilityForInternetConnection;
+
+/**
+ 指定服务器域名初始化
+
+ @param hostName 服务器域名
+ @return NetworkReachability
+ */
 + (instancetype)reachabilityWithHostName:(NSString *)hostName;
+
+/**
+ 指定服务器 IP 初始化
+
+ @param hostAddress IP 地址
+ @return NetworkReachability
+ */
 + (instancetype)reachabilityWithAddress:(const struct sockaddr *)hostAddress;
 
-// 开启以及关闭监听
+
+/**
+ 开启监听
+
+ @return 是否监听成功
+ */
 - (BOOL)startNotifier;
+
+/**
+ 关闭监听
+ */
 - (void)stopNotifier;
 
+
+/**
+ 获取当前网络连接状态
+
+ @return 当前网络连接状态
+ */
 - (NetworkReachabilityStatus)currentReachabilityStatus;
 
 
